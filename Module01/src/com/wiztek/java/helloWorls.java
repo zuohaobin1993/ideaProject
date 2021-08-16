320350c889744dc91815157625fcaf916e4341cd
Ken <821343500@qq.com>
2021-08-16 17:28:33 +0800

    V1.0

---
+++ Module01/src/com/wiztek/java/helloWorls.java
@@ -0,0 +1,34 @@
+package com.wiztek.java;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class helloWorls {
+    public static void main(String[] args) {
+
+        List<String> list =new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("2");
+        System.out.println(list.stream().distinct().collect(Collectors.toList()));
+
+        for (int i = 0; i < list.size()-1; i++) {
+            for (int j = i+1; j < list.size(); j++) {
+                if(list.get(i).equals(list.get(j))){
+                    System.out.println("第"+(i+1)+"个元素"+list.get(i)+"重复");
+                }
+
+            }
+
+        }
+
+
+    }
+
+   public void test(){
+           
+   }
+}
